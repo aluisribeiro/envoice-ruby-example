571bf83d64f0c2bea00867f93e29c617b0e53280
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2017-10-03 21:49:34 -0300

    testando a validacao do item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   belongs_to :envoice
   belongs_to :product
 
+  validates :amount, presence: :true
+
   def total
     self.amount * self.product.price
   end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -8,4 +8,16 @@ class ItemTest < ActiveSupport::TestCase
     assert_equal 200, item.total
   end
 
+  test "should validate the item" do
+    item = Item.new(product: Product.new, envoice: Envoice.new, amount: 2)
+    assert item.valid?
+  end
+
+  test "should validate item without amount" do
+    item = Item.new(product: Product.new, envoice: Envoice.new)
+    refute item.valid?
+  end
+
+
+
 end
