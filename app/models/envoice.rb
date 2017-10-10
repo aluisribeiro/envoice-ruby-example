7c7dd3c8d72b8063c113d2d80fa8325db8123428
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2017-10-10 20:44:53 -0300

    teste do total da nota

--- app/models/envoice.rb
+++ app/models/envoice.rb
@@ -1,7 +1,13 @@
 class Envoice < ApplicationRecord
   belongs_to :customer
+  has_many :items
 
   def total
-    return 150
+    tot = 0
+    self.items.each do |item|
+      tot += item.total
+    end
+    return tot
   end
+
 end
--- test/models/envoice_test.rb
+++ test/models/envoice_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
+require 'minitest/autorun'
 
 class EnvoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should calculate the envoice total" do
+
+    item = Item.new
+
+    item.stub :total, 30 do
+      envoice = Envoice.new
+      envoice.items << item
+      envoice.items << item
+      assert_equal 60, envoice.total
+    end
+
+  end
+
 end
